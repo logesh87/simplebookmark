8514f6bd1326fddb40a03ce4543d1d345cddd1a5
logesh-kumar <phoenix.logu@gmail.com>
2016-07-14 20:35:29 +0530

    Revert "Deleted db file in config"
    This reverts commit 07618b508e0b46f8d1370da791e7051a619bfd73.

---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+	//url : 'mongodb://localhost:27017/bookmarks'
+    //url: 'mongodb://logesh:9894429494@apollo.modulusmongo.net:27017/Ymyri4qi'
+    url: 'mongodb://logesh:9894429494@ds051933.mlab.com:51933/simplebookmarks'
+}
